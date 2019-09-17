7a2088c149c2ae649d859a74bcccbd370bbc7e79
jerabaul29 <jean.rblt@gmail.com>
2019-09-17 16:12:16 +0200

    now compatible with python3 and use only 4 gauges

---
+++ ReadingGauges/ComputerSide/GrabberGaugesOutput_python3/ScriptRunGrabber.py
@@ -0,0 +1,10 @@
+import Grabber
+
+gauge_grabber = Grabber.grabb_serial_values()
+
+gauge_grabber.init()
+gauge_grabber.grabb(10)
+gauge_grabber.convert_grabbed_to_numpy()
+gauge_grabber.plot_grabbed_data()
+gauge_grabber.clean_numpy_dict()
+gauge_grabber.save_cleaned_dict_numpy('test_saving')
